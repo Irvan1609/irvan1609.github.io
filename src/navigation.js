28223f6aef2766a62e101f765834f808d7b4d4d5
Irvan <74125352+Irvan1609@users.noreply.github.com>
2026-09-16 15:42:55 +0800

    Add data quality and fieldbook tools to navigation

--- src/navigation.js
+++ src/navigation.js
@@ -16,7 +16,8 @@ export function installNavigation(){
   if(toolbar)toolbar.hidden=true;
 
   for(const [id,title,ids] of [
-    ['fileMenu','File',['pasteBtn','importBtn','newTxt','importXlsx']],
+    ['fileMenu','File',['pasteBtn','importBtn','importXlsx','newTxt']],
+    ['dataMenu','Data',['undoData','redoData','duplicateDataset','validateDataset','transformData','outlierData','fieldbookTool']],
     ['helpMenu','Help',['dataTemplate','analysisHistory','configureDriveBackup']]
   ]){
     const button=document.createElement('button');
@@ -45,7 +46,7 @@ export function installNavigation(){
   }
 
   function closeMenus(){
-    for(const id of ['fileMenu','helpMenu']){
+    for(const id of ['fileMenu','dataMenu','helpMenu']){
       const panel=document.getElementById(id),button=document.getElementById(id+'Button');
       if(panel)panel.hidden=true;
       if(button)button.setAttribute('aria-expanded','false');
