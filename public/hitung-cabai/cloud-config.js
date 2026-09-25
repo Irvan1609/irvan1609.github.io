dfc66aeba1a2b53c966299ead137ec269e837de4
Irvan <andyirvan1609@gmail.com>
2026-09-25 15:32:01 +0800

    Update cloud-config.js

--- public/hitung-cabai/cloud-config.js
+++ public/hitung-cabai/cloud-config.js
@@ -1,7 +1,7 @@
 export const CHILI_CLOUD_CONFIG={
-  enabled:false,
-  endpoint:'',
-  turnstileSiteKey:'0x4AAAAAAF'+'DFL_8dHZY6bCV6',
+  enabled:true,
+  endpoint:'https://hitung-cabai-api.andyirvan1609.workers.dev',
+  turnstileSiteKey:'0x4AAAAAAFDFL_8dHZY6bCV6',
   modelVersion:'heuristic-color-v1',
   maxUploadBytes:850000
 };
@@ -9,7 +9,7 @@ export const CHILI_CLOUD_CONFIG={
 export function cloudContributionReady(){
   return Boolean(
     CHILI_CLOUD_CONFIG.enabled &&
-    /^https:\/\//i.test(CHILI_CLOUD_CONFIG.endpoint) &&
+    /^https:\/\/\/?/i.test(CHILI_CLOUD_CONFIG.endpoint) &&
     CHILI_CLOUD_CONFIG.turnstileSiteKey
   );
 }
