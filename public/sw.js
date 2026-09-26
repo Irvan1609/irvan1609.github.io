b446c8b69c08617c0927d82af3b17268405f9712
Irvan <74125352+Irvan1609@users.noreply.github.com>
2026-09-26 20:09:48 +0800

    deploy: refresh calm game interface cache

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const VERSION='20260926-pengukur-v3';
+const VERSION='20260926-calm-pengukur-v4';
 const CORE_CACHE='agrotik-core-'+VERSION;
 const RUNTIME_CACHE='agrotik-runtime-'+VERSION;
 const THIRD_PARTY_CACHE='agrotik-third-party-'+VERSION;
